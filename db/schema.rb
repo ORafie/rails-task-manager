8e10663a3cec707c264fbc4426b7efe350dc1933
olena rafieienko <rafeyenko@yahoo.com>
2022-12-06 18:26:08 +0100

    add false to completed boolean

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_06_170545) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_06_172056) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.string "details"
-    t.boolean "completed"
+    t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
